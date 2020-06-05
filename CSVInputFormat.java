5e0dce4140de962fcef05893375fb276d585418f
Kamolchanok Tangsri <kamolchanok.tangsri@gmail.com>
2020-06-05 08:55:43 +0700

    Update Flexible parameters

--- CSVInputFormat.java
+++ CSVInputFormat.java
@@ -23,33 +23,29 @@ public class CSVInputFormat {
 		      System.exit(0);
 		 }
 		
-		String warehouseLocation = new File("spark-warehouse").getAbsolutePath();
-		LOG.info(warehouseLocation);
-		
-		SparkSession spark = SparkSession
-//				  .builder().master("spark://172.19.0.2:7077")
-				 .builder().master("local[*]")
-				  .appName("Java Spark to read embedded new line")
-//				  .config("spark.sql.warehouse.dir", warehouseLocation)
-				  .config("spark.authenticate", "true")
-				  .config("spark.authenticate.secret", "aaa")
-//				  .enableHiveSupport()
-				  .getOrCreate();
 		String csvFile = args[0];
 		String DBTable = args[1];
+		String sparkMasterUrl = args[2];
+		String warehouseLocation = (args[3] != null) ? args[3] : new File("spark-warehouse").getAbsolutePath();
+//		LOG.info(warehouseLocation);
+		LOG.info("TEsting");
+		SparkSession spark = SparkSession
+				 .builder().master(sparkMasterUrl)
+				  .appName("Java Spark to read embedded new line")
+				  .config("spark.sql.warehouse.dir", warehouseLocation)
+				  .enableHiveSupport()
+				  .getOrCreate();
+		
 		Dataset<Row> df =  spark.read().format("csv").option("wholeFile", "true").option("sep", ",").option("quote","\"").option("escape","\"").option("multiLine", "true").option("inferSchema", "true").option("header", "true").load(csvFile);
-//		spark.read().option("wholeFile", true).option("sep", ",").option("quote","\"").option("escape","\"").option("multiLine", true).option("inferSchema", "true").option("header", true).csv(csvFile).write().format("orc").save(DBTable);
-		df.select(col("name")).show();
 		df.show();
 		df.printSchema();
-//		df.registerTempTable("orc_table");
 //		df.select("id","name").write().format("orc").save(DBTable);
 		
 		Dataset<Database> dfDB =  spark.catalog().listDatabases();
 		dfDB.show();
-		dfDB.select(col("locationUri")).show();
-		spark.sql("CREATE DATABASE sandbox_test");
-		spark.catalog().listDatabases().show();
+//		dfDB.select(col("locationUri")).show();
+//		spark.sql("CREATE TABLE "+ DBTable);
+//		spark.catalog().listDatabases().show();
 		LOG.info(warehouseLocation);
 		
 	}
